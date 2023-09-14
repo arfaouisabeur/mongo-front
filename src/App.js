7c3e239b758f6cda197ad6e37ec331298c50e41b
unknown <arfaouisaber140@gmail.com>
2023-09-14 12:36:28 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [msg, setMsg] = useState('');
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://98.66.136.102:80/", {
+        msg
+      });
+      // Add code to handle success if needed
+    } catch (error) {
+      console.error(error);
+      // Add code to handle errors if needed
+    }
+  };
+  const [genres, setgenres] = useState([]);
+
+  useEffect(() => {
+    fetch('http://98.66.136.102:80/')
+      .then((response) => response.json())
+      .then((data) => setgenres(data))
+      .catch((error) => console.error('Error fetching data:', error));
+
+      
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cont'>
+      <form onSubmit={submit}>
+        <textarea
+          name="text"
+          value={msg}
+          onChange={(e) => { setMsg(e.target.value) }}
+          placeholder='hiiiiii'
+        ></textarea>
+        <input type='submit' value='submit' />
+      </form>
+      <h1>genres List</h1>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+
+
+/*import React, { useEffect, useState } from 'react';
+
+function BookList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) => console.error('Error fetching data:', error));
+
+      
+  }, []);
+  console.log(books)
+  return (
+    <div>
+      <h1>Book List</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;*/
